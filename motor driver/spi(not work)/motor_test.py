f1452fdbb614be26d0f55275e3fbc84c0b08861c
Yifan Zhang <yiz133@pitt.edu>
2020-08-20 16:25:09 -0400

    Py3

---
+++ motor driver/spi(not work)/motor_test.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 23 14:08:43 2020
+
+@author: user
+"""
+#import os
+#os.getcwd()
+#os.chdir('C:\Users\user\Desktop\py code\Python3\MMC5603')
+
+import StepperDriver_py3
+import time
+import board
+import busio
+import digitalio
+#### SPI SETUP ####
+# chip select pin:
+csp = 8
+# clock speed in Hz:
+cs = 3000000
+
+spiMode = 0
+# Bit Order
+bitOrder = 'MSB'
+
+current = 2500
+# stepper motor step size
+stepMode = 'MicroStep4'
+# stepper motor step delay
+stepdelay = 0.001
+
+## Create HPSD object
+spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+sd_cs = digitalio.DigitalInOut(board.C0)
+sd = StepperDriver_py3.HighPowerStepperDriver(spi, sd_cs)
+
+#sd.setupSPI()
+#sd.setupSPI(csp, cs, spiMode, bitOrder)
+# give driver time to power up?
+time.sleep(1)
+
+#### MOTOR SETUP ####
+
+# set decay mode: auto mixed decay recommended by documentation
+sd.setDecayMode('AutoMixed')
+
+## set current limit
+sd.setCurrentLimit(current)
+
+## set step mode
+sd.setStepMode(stepMode)
+
+## enable driver
+sd.enableDriver()
+sd.setDirection(1)
+
+sd.step(500, stepdelay)
+
+sd.disableDriver()
