605b5d1bf8b88b12e855ce1c08482952093bfc0c
Yifan Zhang <yiz133@pitt.edu>
2024-03-11 12:50:13 -0400

    update

--- multiprocessing/hello.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov 18 17:50:09 2022
-
-@author: Administrator
-"""
-
-print("Hello from my Python script")
---
+++ multiprocessing/threadings.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 28 11:54:34 2024
+
+@author: Administrator
+"""
+
+# multi_threaded.py
+import time
+from threading import Thread
+
+COUNT = 50000000
+
+def countdown(n):
+    while n>0:
+        n -= 1
+
+t1 = Thread(target=countdown, args=(COUNT//2,))
+t2 = Thread(target=countdown, args=(COUNT//2,))
+
+start = time.time()
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+end = time.time()
+
+print('Time taken in seconds -', end - start)
